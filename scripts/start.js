c7b1e9080858818c7b5f926b7c5d62fa36ca55d0
kergma@gmail.com <kergma@gmail.com>
2023-02-19 22:19:38 +0200

    with css

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,12 @@
 const cont = document.getElementById('root');
 const root = ReactDOM.createRoot(cont);
 function App() {
-  return /*#__PURE__*/React.createElement("h1", null, "Hello from start.js");
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("header", null, /*#__PURE__*/React.createElement("nav", null)), /*#__PURE__*/React.createElement("main", null, /*#__PURE__*/React.createElement("aside", {
+    class: "left-side"
+  }), /*#__PURE__*/React.createElement("div", {
+    class: "main-div"
+  }), /*#__PURE__*/React.createElement("aside", {
+    class: "right-side"
+  })), /*#__PURE__*/React.createElement("footer", null));
 }
 root.render( /*#__PURE__*/React.createElement(App, null));
--- scripts/start.jsx
+++ scripts/start.jsx
@@ -3,9 +3,19 @@ const cont = document.getElementById('root');
 const root = ReactDOM.createRoot(cont);
 function App() {
     return (
-        <h1>
-            Hello from start.js
-        </h1>
+        <div>
+            <header>
+                <nav></nav>
+            </header>
+            <main>
+                <aside class="left-side"></aside>
+                <div class="main-div"></div>
+                <aside class="right-side"></aside>
+            </main>
+            <footer>
+
+            </footer>
+        </div>
     );
 }
 root.render(<App />);
