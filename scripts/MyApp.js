433f7cbe159aa1b599bc738b39fd10104579631a
kergma@gmail.com <kergma@gmail.com>
2023-02-19 20:06:27 +0200

    module

---
+++ scripts/MyApp.js
@@ -0,0 +1,6 @@
+
+
+function MyApp() {
+    return <h1>Hello, world!</h1>;
+}
+export default MyApp;
---
+++ scripts/start.js
@@ -0,0 +1,10 @@
+
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+
+import App from "./App";
+//const head1 = <h1>Hello!</h1>;
+root.render(<App />);
+
