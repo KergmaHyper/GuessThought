73f9968bd6a6e0d94532a01e394d7a25d9296dbf
kergma@gmail.com <kergma@gmail.com>
2023-02-19 23:33:50 +0200

    test1

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,18 @@
+//import React from "react";
+//import table from "./table";
+
 const cont = document.getElementById('root');
 const root = ReactDOM.createRoot(cont);
+function table(a, b, c) {
+  return /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, a), /*#__PURE__*/React.createElement("td", null, b), /*#__PURE__*/React.createElement("td", null, c)), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, a + 1), /*#__PURE__*/React.createElement("td", null, b + 1), /*#__PURE__*/React.createElement("td", null, c + 1)));
+}
 function App() {
   return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("header", null, /*#__PURE__*/React.createElement("nav", null)), /*#__PURE__*/React.createElement("main", null, /*#__PURE__*/React.createElement("aside", {
-    class: "left-side"
+    className: "left-side"
   }), /*#__PURE__*/React.createElement("div", {
-    class: "main-div"
-  }), /*#__PURE__*/React.createElement("aside", {
-    class: "right-side"
+    className: "main-div"
+  }, table(1, 2, 3)), /*#__PURE__*/React.createElement("aside", {
+    className: "right-side"
   })), /*#__PURE__*/React.createElement("footer", null));
 }
 root.render( /*#__PURE__*/React.createElement(App, null));
--- scripts/start.jsx
+++ scripts/start.jsx
@@ -1,6 +1,31 @@
+//import React from "react";
+//import table from "./table";
+
 
 const cont = document.getElementById('root');
 const root = ReactDOM.createRoot(cont);
+function table(a, b, c) {
+    return (
+        <table>
+            <tr>
+                <td>{a}</td>
+                <td>{b}</td>
+                <td>{c}</td>
+            </tr>
+            <tr>
+                <td>{a + 1}</td>
+                <td>{b + 1}</td>
+                <td>{c + 1}</td>
+            </tr>
+            <tr>
+                <td>{a + 2}</td>
+                <td>{b + 2}</td>
+                <td>{c + 2}</td>
+            </tr>
+        </table>
+    );
+}
+
 function App() {
     return (
         <div>
@@ -8,9 +33,13 @@ function App() {
                 <nav></nav>
             </header>
             <main>
-                <aside class="left-side"></aside>
-                <div class="main-div"></div>
-                <aside class="right-side"></aside>
+                <aside className="left-side"></aside>
+                <div className="main-div">
+                    {
+                        table(1, 2, 3)
+                    }
+                </div>
+                <aside className="right-side"></aside>
             </main>
             <footer>
 
