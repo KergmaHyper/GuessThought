42e5bcc84096463969f13e5843e475b308bd1ea1
kergma@gmail.com <kergma@gmail.com>
2023-02-21 16:02:44 +0200

    work 2.2

--- scripts/start.jsx
+++ scripts/start.jsx
@@ -8,13 +8,14 @@ const headerR = ReactDOM.createRoot(headerEl);
 
 class Hello extends React.Component {
     render() {
-        return <sup>
+        return (<sup>
             Hello first Name: '{this.props.name}'  second Name: '{this.props.secName}'
-        </sup>
+        </sup>)
     }
 }
+Hello.defaultProps = { name: 'Sandra', secName: 'Bullock' };
 
-class App extends React.Component {
+class GetTime extends React.Component {
     render() {
         return (
             <sup>
@@ -24,23 +25,66 @@ class App extends React.Component {
     }
 }
 
+class GetButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.press = this.press.bind(this);
+    }
+
+    press() {
+        let msg = "msg: " + this.props.msg + " ";
+        alert(msg);
+        console.log(this);
+    }
+
+
+    render() {
+        return (
+            <button onClick={this.press}> {this.props.caption}</button>
+        )
+    }
+}
+GetButton.defaultProps = { caption: "Press", msg: "Test msg." };
+
 class HeaderSet extends React.Component {
+
     render() {
         return (
             <div>
-                <h2><App /></h2>
+                <h2><GetTime /></h2>
             </div>
         )
     }
 }
 
+let user = [];
+user[0] = { name: "Sandra", secName: "Bullock" };
+user[1] = { name: "Alex", secName: "Filtch" };
+user[2] = { name: "Bob", secName: "Brown" };
+user[3] = { name: "Charly", secName: "Smith" };
+
+class GetUser extends React.Component {
+    render() {
+        return (
+            <p>
+                <GetTime />
+                <Hello name={user[this.props.num].name} secName={user[this.props.num].secName} />
+                <GetButton caption={user[this.props.num].name} msg={'Hello ' + user[this.props.num].name} />
+            </p>
+        );
+    }
+}
+GetUser.defaultProps = { num: "0" }
+
 class BodySet extends React.Component {
     render() {
         return (
             <div>
-                <p><App /> <Hello name="Alex" secName="Fich" /></p>
-                <p><App /> <Hello name="Bob" secName="Brown" /></p>
-                <p><App /> <Hello name="Charly" secName="Smit" /></p>
+                <GetUser num="1" />
+                <GetUser num="2" />
+                <GetUser num="3" />
+                <GetUser />
             </div>
         )
     }
