132a0cc746f403a532377bae631dbc495c8f7c88
kergma@gmail.com <kergma@gmail.com>
2023-02-21 11:16:33 +0200

    ver 2.1

--- scripts/start.jsx
+++ scripts/start.jsx
@@ -14,7 +14,11 @@ function App() {
     return (
         <div id="subroot">
             <header id="header">
+
                 <nav id="nav"></nav>
+                <h2>
+                    {new Date().toLocaleTimeString()}
+                </h2>
             </header>
             <main id="main">
                 <aside className="left-side"></aside>
@@ -31,3 +35,17 @@ function App() {
 
 root.render(<App />);
 
+
+const headerEl = document.getElementById("header");
+function setHeader() {
+
+    headerEl.render(
+        <div>
+            <h2>
+                {new Date().toLocaleTimeString()}
+            </h2>
+        </div>
+    )
+}
+
+setHeader();
