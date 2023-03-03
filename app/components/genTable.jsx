0b22c28d6289ca0c6685d9bdd42bc7ae944a7f0e
kergma@gmail.com <kergma@gmail.com>
2023-03-03 04:41:35 +0200

    table

--- app/components/genTable.jsx
+++ app/components/genTable.jsx
@@ -2,15 +2,36 @@ import React from "react";
 import "../../css/flex.css";
 
 function GenTable(props) {
-
+    const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     return (
         <div>
             <h2>Generate Table Page</h2>
 
             <ul className="flex between">
-                {props.symbols.map((item) => <li key={item} >{item}</li>)}
+                {props.symbols.map((item) => <li key={item} className="no-marker list1">{item}</li>)}
             </ul>
-            <h2>{props.symbols[props.symbol]}</h2>
+            <table>
+                <tbody>
+                    {rows.map((itemR, i) => {
+                        return (
+                            <tr key={itemR}>
+                                {columns.map((ItemC, j) => {
+                                    return (
+                                        <td key={ItemC}>
+                                            {itemR * 10 + ItemC}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+
+
+
+            <h1>{props.symbols[props.symbol]}</h1>
         </div>)
 }
 
